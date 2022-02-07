9ede08c72d9d5a30d86e90e7f98c866296794d5e
cse18107 <cse18107@cemk.ac.in>
2022-02-07 09:24:51 +0530

    working on productRouter and productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,112 @@
-exports.getAllProducts = (req,res)=>{
+const Product = require("../model/productModel");
+
+//CREATE A PRODUCT
+exports.createProduct = async (req, res) => {
+  const body = req.body;
+  try {
+    const product = await Product.create(body);
     res.status(200).json({
-        message:'Route is working fine',
-    })
-}
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+};
+
+// GET ALL PRODUCTS
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+      status: "Success",
+      data: {
+        products,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+};
+
+//GET A PRODUCT
+exports.getAProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    res.status(200).json({
+      status: "Success",
+      message: {
+        product,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+};
+
+// UPDATE A PRODUCT
+exports.updateProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const product = await Product.findById(id);
+    if (product) {
+      const updatedProduct = await Product.findByIdAndUpdate(id, product);
+      res.status(200).json({
+        status: "Success",
+        message: {
+          updatedProduct,
+        },
+      });
+    } else {
+      res.status(500).json({
+        status: "Failed",
+        message: "Product not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: error.stack,
+    });
+  }
+};
+
+// DELETE A PRODUCT
+exports.deleteAProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (product) {
+      const deletedProduct = await Product.findByIdAndDelete(id);
+      res.status(200).json({
+        status: "Success",
+        message: {
+          deletedProduct,
+        },
+      });
+    } else {
+      res.status(500).json({
+        status: "Failed",
+        message: "Product not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,14 @@
-const express= require('express');
-const { getAllProducts } = require('../controllers/productController');
+const express = require("express");
+const productRouters = require("../controllers/productController");
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts)
-
+router.route("/products").get(productRouters.getAllProducts);
+router.route("/products/new").post(productRouters.createProduct);
+router
+  .route("/products/:id")
+  .get(productRouters.getAProduct)
+  .patch(productRouters.updateProduct)
+  .delete(productRouters.deleteAProduct);
 
 module.exports = router;
