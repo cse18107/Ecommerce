4d25a2f05d08a2bc3c94fe551c6de1b2794eacee
cse18107 <cse18107@cemk.ac.in>
2022-02-12 07:49:09 +0530

    worked on getting user details

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -126,3 +126,15 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
   await user.save();
   sendToken(user,200,res);
 });
+
+// Get User Detail
+exports.getUserDetails = catchAsyncErrors(async(req,res,next)=>{
+  
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success:true,
+    user
+  })
+  
+})
