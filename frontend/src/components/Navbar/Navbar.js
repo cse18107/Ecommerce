0635c41ec9d69c19965c1505438e31847a36119e
cse18107 <cse18107@cemk.ac.in>
2022-02-22 00:06:16 +0530

    working on frontend

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Navbar.css';
+import {Link, NavLink} from 'react-router-dom';
+
+export const Navbar = () => {
+  return (
+    <div className="navbar-body">
+        <div className="navbar-container">
+            <Link to={"/"} className="navbar-logo">Ecommarce</Link>
+            <div className="navbar-links">
+                <NavLink to={"/"} className="n-links">Home</NavLink>
+                <NavLink to={"/product"} className="n-links">Product</NavLink>
+                <NavLink to={"/contact"} className="n-links">Contact</NavLink>
+                <NavLink to={"/about"} className="n-links">About</NavLink>
+            </div>
+            <div className="navbar-icons">
+                <Link to={"/"} className="n-icons">Search</Link>
+                <Link to={"/"} className="n-icons">Cart</Link>
+                <Link to={"/"} className="n-icons">Account</Link>
+            </div>
+
+        </div>
+    </div>
+  )
+}
