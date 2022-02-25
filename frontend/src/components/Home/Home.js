ebaf47240a481e53059564254055e956c2508805
cse18107 <cse18107@cemk.ac.in>
2022-02-25 11:02:12 +0530

    working on ui part

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
 import './Home.css';
 import Product from './Product.js';
+import MetaData from '../../components/layout/MetaData'
 
 const product = {
-    name:"Blue Tshirt",
+    name:"Blue T-shirt",
     images:[{url: "https://m.media-amazon.com/images/I/81XUSO4a8WL._UY445_.jpg"}],
     price:'$3000',
     _id:"saikat"
@@ -12,15 +13,24 @@ const product = {
 const Home = () => {
   return (
     <Fragment>
+        <MetaData title="ECOMMERCE"/>
         <div className="banner">
             <p>Welcome to Ecommerce</p>
             <h1>FIND AMAZING PRODUCTS BELOW</h1>
-            <a href='#container'>
+            <a href='#homeHeading'>
                 <button>Scroll</button>
             </a>
         </div>
-        <h2 className="homeHeading">Featured Products</h2>
+        <h2 className="homeHeading" id="homeHeading">Featured Products</h2>
         <div className="container" id="container">
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
             <Product product={product}/>
         </div>
     </Fragment>
