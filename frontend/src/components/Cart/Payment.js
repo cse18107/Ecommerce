5eb8b5f32cb1df7e7d7b9894019740dbed0cbf1c
cse18107 <cse18107@cemk.ac.in>
2022-04-19 09:23:22 +0530

    add payment

--- frontend/src/components/Cart/Payment.js
+++ frontend/src/components/Cart/Payment.js
@@ -7,7 +7,7 @@ import { useAlert } from "react-alert";
 import {
   CardNumberElement,
   CardCvcElement,
-  CardExpireElement,
+  CardExpiryElement,
   useStripe,
   useElements,
 } from "@stripe/react-stripe-js";
@@ -16,13 +16,83 @@ import "./payment.css";
 import CreditCardIcon from "@material-ui/icons/CreditCard";
 import EventIcon from "@material-ui/icons/Event";
 import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import { useNavigate } from 'react-router-dom';
 
 const Payment = () => {
   const orderInfo = JSON.parse(sessionStorage.getItem("orderInfo"));
 
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const stripe = useStripe();
+  const elements = useElements();
   const payBtn = useRef(null);
 
-  const submitHandler = () => {};
+  const { shippingInfo, cartItem } = useSelector((state) => state.cart);
+  const { user } = useSelector((state) => state.user);
+  // const { error } = useSelector((state) => state.newOrder);
+
+  const paymentDate = {
+    amount: Math.round(orderInfo.totalPrice * 100),
+  };
+
+  const submitHandler =async (e) => {
+    e.preventDefault();
+
+    payBtn.current.disabled = true;
+
+  
+    try{
+      const config={
+        headers:{
+          "Content-Type":"application/json",
+        },
+      };
+      const { data } = await axios.post("/api/v1/payment/process",
+      paymentDate,
+        config
+      );
+
+      console.log(data);
+
+      const client_secret = data.client_secret;
+
+      if(!stripe || !elements) return;
+
+      const result = await stripe.confirmCardPayment(client_secret, {
+        payment_method: {
+          card: elements.getElement(CardNumberElement),
+          billing_details: {
+            name: useRef.name,
+            email: useRef.email,
+            address: {
+              line1: shippingInfo.address,
+              city: shippingInfo.city,
+              state: shippingInfo.state,
+              postal_code: shippingInfo.pinCode,
+              country: shippingInfo.country,
+            },
+          },
+        },
+      });
+
+      if(result.error) {
+        payBtn.current.disabled = false;
+
+        alert.error(result.error);
+      }else {
+        if(result.paymentIntent.status === "succeeded") {
+          navigate('/success');
+        }else {
+          alert.error("There's some issue while processing payment");
+        }
+      }
+    }catch(error){
+      payBtn.current.disabled = false;
+      alert.error(error.response.data.error);
+    }
+  };
 
   return (
     <Fragment>
@@ -33,15 +103,15 @@ const Payment = () => {
           <Typography>Card Info</Typography>
           <div>
             <CreditCardIcon />
-            {/* <CardNumberElement className="paymentInput" /> */}
+            <CardNumberElement className="paymentInput" />
           </div>
           <div>
             <EventIcon />
-            {/* <CardExpireElement className="paymentInput" /> */}
+            <CardExpiryElement className="paymentInput" />
           </div>
           <div>
             <VpnKeyIcon />
-            {/* <CardCvcElement className="paymentInput" /> */}
+            <CardCvcElement className="paymentInput" />
           </div>
 
           <input
